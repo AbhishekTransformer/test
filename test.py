5dfe5216e35987f77bfc5fe7fd09dd0ce678edb9
Abhishek Kumar <126065901+AbhishekTransformer@users.noreply.github.com>
2024-05-06 16:52:11 +0200

    Updated with ll

--- test.py
+++ test.py
@@ -1,2 +1,48 @@
-print("This is to check artifact creation")
-print("A new line to add")
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+class AS:
+    def __init__(self):
+        self.head = None
+
+    def make_as(self, *data):
+        for i in data:
+            node = Node(i)
+            if self.head == None:
+                self.head = node
+            else:
+                itr = self.head
+                while itr.next:
+                    itr = itr.next
+                itr.next = node
+    def show_as(self):
+        itr = self.head
+        print("AS--->", end="")
+        while itr:
+            print(itr.data, end='->')
+            itr = itr.next
+    def rev_as(self):
+        curr = self.head
+        prev = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        self.head = prev
+
+    def sort_as(self):
+        pass
+
+
+obj = AS()
+obj.make_as(1,2,3,4,5,6,7,8,9)
+obj.show_as()
+obj.rev_as()
+print()
+obj.show_as()
+
+obj.sort_as()
